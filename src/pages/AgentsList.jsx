e0dccc3c257f45c25cce28e454444cc35488b0c0
Muhasin141 <muhasinalikhan@gmail.com>
2025-12-19 11:41:23 +0530

    Update AgentsList.jsx

--- src/pages/AgentsList.jsx
+++ src/pages/AgentsList.jsx
@@ -9,11 +9,7 @@ const AgentsList = () => {
   const navigate = useNavigate();
 
   const handleDelete = async (id) => {
-    const confirmDelete = window.confirm(
-      "Are you sure you want to delete this agent?"
-    );
-
-    if (!confirmDelete) return;
+  
 
     try {
       await deleteAgent(id);
@@ -87,4 +83,5 @@ const AgentsList = () => {
 };
 
 export default AgentsList;
+
 
