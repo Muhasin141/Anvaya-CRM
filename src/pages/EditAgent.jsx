481574bf231886fd541bef1c259677182464852b
Muhasin141 <muhasinalikhan@gmail.com>
2025-12-20 10:31:29 +0530

    Update EditAgent.jsx

--- src/pages/EditAgent.jsx
+++ src/pages/EditAgent.jsx
@@ -44,12 +44,9 @@ const EditAgent = () => {
   };
 
   return (
-    <div className="d-flex min-vh-100">
-      {/* Sidebar */}
-      <SideBar />
-
-      {/* Main content */}
-      <main className="flex-fill p-3 p-md-4" style={{ minWidth: 0 }}>
+  <div className="d-flex flex-column flex-md-row min-vh-100">
+  <SideBar />
+  <main className="flex-fill p-3 p-md-4">
         <h1 className="mb-4">Edit Agent</h1>
 
         <div className="card shadow-sm">
@@ -104,4 +101,5 @@ const EditAgent = () => {
 };
 
 export default EditAgent;
+
 
